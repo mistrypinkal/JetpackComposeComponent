c0ed13bfbbbe19da2b7d77cb3003a9ac1b44d205
Pinkal Mistry <pinkald.mistry@gmail.com>
2022-06-15 15:10:08 +0400

    Added the Jetpack lib

--- app/src/main/java/com/pm/jetpackcomposecomponent/MainActivity.kt
+++ app/src/main/java/com/pm/jetpackcomposecomponent/MainActivity.kt
@@ -14,12 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeComponentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Greeting(name = "Pinkal Mistry")
         }
     }
 }
